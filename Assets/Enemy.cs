b01e84a8e3e4e236c18427d3eddd19aeaa2c6765
Robert Lafferty <rclafferty@ksu.edu>
2019-03-10 07:16:59 -0500

    Player Movement, Enemy Movement, SIMPLE Graphics

---
+++ Assets/Enemy.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Enemy : MonoBehaviour
+{
+    GameObject target;
+    Rigidbody2D thisRigidbody;
+
+    Vector2 targetPos;
+    Vector2 thisPos;
+
+    float xdiff;
+    float ydiff; // for 3D, use zdiff
+
+    const float SPEED = 3f;
+    const float DIST_THRESHHOLD = 0.5f;
+
+    // Debug Only
+    Text diffText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        target = GameObject.Find("Player");
+        // target = GameObject.FindGameObjectWithTag("Player");
+
+        thisRigidbody = GetComponent<Rigidbody2D>();
+
+        diffText = GameObject.Find("DiffText").GetComponent<Text>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        targetPos = target.transform.position;
+        thisPos = this.transform.position;
+
+        xdiff = targetPos.x - thisPos.x;
+        ydiff = targetPos.y - thisPos.y;
+
+        diffText.text = "X diff = " + xdiff + "    ydiff = " + ydiff;
+
+        /*if (Mathf.Abs(xdiff) > DIST_THRESHHOLD || Mathf.Abs(ydiff) > DIST_THRESHHOLD)
+        {
+            //transform.Translate(new Vector2(xdiff, ydiff) * SPEED);
+            
+
+        }*/
+        if (Mathf.Abs(xdiff) < DIST_THRESHHOLD && Mathf.Abs(ydiff) < DIST_THRESHHOLD)
+        {
+            //transform.Translate(new Vector2(xdiff, ydiff) * SPEED);
+
+            xdiff = 0;
+            ydiff = 0;
+        }
+
+        Vector2 direction = new Vector2(xdiff, ydiff);
+        direction.Normalize();
+
+        thisRigidbody.velocity = direction * SPEED;
+    }
+}
---
+++ Assets/Player.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    Rigidbody2D thisRigidbody;
+    const float SPEED = 8f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        thisRigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float xMovement = Input.GetAxis("Horizontal");
+        float yMovement = Input.GetAxis("Vertical");
+
+        Debug.Log(xMovement + " " + yMovement);
+
+        float speedModifier = 1;
+
+        // Move
+        thisRigidbody.velocity = new Vector2(xMovement * SPEED * speedModifier, yMovement * SPEED * speedModifier);
+    }
+}
